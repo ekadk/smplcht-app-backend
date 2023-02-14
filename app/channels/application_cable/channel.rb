a054a7d48390a5f6a707fd59f07af26c545090c3
ekadk <ekadharmakusuma3@gmail.com>
2023-02-15 05:09:32 +0800

    add: models, controllers, migrations, schema

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,15 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_user
+    end
+
+    private
+
+    def find_verified_user
+  if verified_user = User.find_by(id)
+    end
   end
 end
